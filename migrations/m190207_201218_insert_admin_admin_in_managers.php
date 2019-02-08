e81b600031a2872f436ae5f7176f932980ba31d4
Bessonov Alexander <bessonomap@yandex.ru>
2019-02-08 11:54:41 +0300

    add vendor and migration

---
+++ migrations/m190207_201218_insert_admin_admin_in_managers.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190207_201218_insert_admin_admin_in_managers
+ */
+class m190207_201218_insert_admin_admin_in_managers extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190207_201218_insert_admin_admin_in_managers cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190207_201218_insert_admin_admin_in_managers cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
